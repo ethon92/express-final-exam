dfc84d271028db855072fadbe728b21bf44fcf85
ethon92 <e19920526@gmail.com>
2021-12-15 20:43:29 +0800

    Create about/contact/portfolio page.

--- app.js
+++ app.js
@@ -2,15 +2,34 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
+const req = require('express/lib/request')
+const res = require('express/lib/response')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
+// 設定首頁
 app.get('/', (req, res) => {
   res.render('home')
 })
+
+// 設定about頁面
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+
+// 設定portfolio頁面
+app.get('/portfolio', (req, res) => {
+  res.render('portfolio')
+})
+
+// 設定contact頁面
+app.get('/contact', (req, res) => {
+  res.render('contact')
+})
+
 
 app.listen(port, () => {
   console.log(`web is running on http://localhost:${port}`)
